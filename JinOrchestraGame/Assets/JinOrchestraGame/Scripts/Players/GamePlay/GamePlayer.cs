332d87a22cbc56fb9d820f1354523c147c391707
huahai <279598616@qq.com>
2020-04-03 16:49:56 +0800

    <Feat>():客户端和host都能生成相同的方向图片。

--- JinOrchestraGame/Assets/JinOrchestraGame/Scripts/Players/GamePlay/GamePlayer.cs
+++ JinOrchestraGame/Assets/JinOrchestraGame/Scripts/Players/GamePlay/GamePlayer.cs
@@ -1,4 +1,5 @@
 ﻿using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,21 @@ namespace JO
         public int index;
         [SyncVar]
         public uint score;
+        [SyncVar(hook = nameof(OnDirectionIndexChanged))]
+        public int directionIndex;
+        
+        public GameObject directionsPool;
+        public GameObject spawningPlace;
+        public GameObject bottomCenterPoint;
+
+        void OnDirectionIndexChanged(int oldDirectionIndex, int newDirectionIndex)
+        {
+            GameObject go = Resources.Load<GameObject>("Prefabs/Directions/Directions_" + newDirectionIndex);
+            go = GameObject.Instantiate(go);
+            go.transform.parent = directionsPool.transform;
+            go.transform.localPosition = spawningPlace.transform.localPosition;
+            Debug.Log("距离底部还有多远：" + Vector3.Distance(go.transform.localPosition, bottomCenterPoint.transform.localPosition));
+        }
 
         public override void OnStartClient()
         {
@@ -19,6 +35,30 @@ namespace JO
             int x = index * 6;
             int y = 0;
             transform.localPosition = new Vector3(x, y, 0);
+
+
+            //InvokeRepeating(nameof(GenerateDirections), 1, 1);//每隔一秒调用一次
+        }
+
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+            InvokeRepeating(nameof(GenerateDirectionIndex), 1, 1);//每隔一秒调用一次
+        }
+
+        [ServerCallback]
+        void GenerateDirectionIndex()
+        {
+            directionIndex = UnityEngine.Random.Range(0, 4);
+        }
+        
+        void GenerateDirections()
+        {
+            GameObject go = Resources.Load<GameObject>("Prefabs/Directions/Directions_" + directionIndex);
+            go = GameObject.Instantiate(go);
+            go.transform.parent = directionsPool.transform;
+            go.transform.localPosition = spawningPlace.transform.localPosition;
+            Debug.Log("距离底部还有多远：" + Vector3.Distance(go.transform.localPosition, bottomCenterPoint.transform.localPosition));
         }
 
         private void OnGUI()
